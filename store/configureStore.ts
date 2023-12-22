fe7683599f18044bdf35bbe9e6203775a0c820c7
Christopher Bryan <chrisbryann@Christophers-MacBook-Pro.local>
2023-12-22 10:21:09 -0800

    initial setup for redux toolkit for user

---
+++ store/configureStore.ts
@@ -0,0 +1,41 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist';
+import userReducer from './User/userSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  //   blacklist: ['classes'],
+};
+
+const rootReducer = combineReducers({
+  auth: userReducer,
+});
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedRootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
